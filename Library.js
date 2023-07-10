fcf1f1af3126b0648b80e501f965f258e42a8607
BilalAbouBaraki <bilalaboub1452001@gmail.com>
2023-07-10 17:31:26 +0300

    First Assignemnt

--- Library.js
+++ Library.js
@@ -6,6 +6,18 @@ class Library extends Books {
   addBook(title,author,pages){
     books_1= new Book(title,author,pages)
     this.books.push(books_1)
+    console.log(books_1)
     return books_1
   }
+  findBook(title) {
+    for (i = 0; i < this.books.length; i++) {
+        if (title == this.books[i]) {
+            return this.books[i]
+        }
+        else {
+            console.log('Book not found')
+        }
+    }
+}
+
 }
