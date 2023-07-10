c9b431b018efdac5fcdb021c0eb2a915d6cb09d1
BilalAbouBaraki <bilalaboub1452001@gmail.com>
2023-07-10 17:56:19 +0300

    First Assignemnt

--- Books.js
+++ Books.js
@@ -4,7 +4,13 @@ class Book {
     this.title=title;
     this.author=author;
     this.pages=pages;
-   
+
+  }
+  addBook(title,author,pages)
+  {
+    books_1 = new Book(title, author, pages)
+    this.books.push(books_1)
+    console.log(books_1)
   }
   read()
   {
@@ -13,3 +19,5 @@ class Book {
   }
   
 }
+book_1 = new Book("Anna Karenina","	Leo Tolstoy",	864)
+book_2 = new Book("To Kill a Mockingbird","Harper Lee",	281)
